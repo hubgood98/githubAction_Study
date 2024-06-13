308c6306f7ff865be3d68c90e9ddfcac4048094e
huijun98 <heej1998@gmail.com>
2024-06-13 19:15:50 +0900

    Initial commit

---
+++ src/main/java/org/study/githubactionsstudy/GithubActionsStudyApplication.java
@@ -0,0 +1,13 @@
+package org.study.githubactionsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/githubactionsstudy/GithubActionsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.githubactionsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
